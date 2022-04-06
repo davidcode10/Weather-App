a4ceba656cf680d4df69804e88f17536ce8a7d02
davidcode10 <davidedson2030@outlook.com>
2022-04-06 16:06:27 +0200

    App done

---
+++ Weather-App/Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by David Santiago on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
